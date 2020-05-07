ac379eb801e647bfe5203cf2efb35b7fbfb22d76
J Perrin <perrinjack96@gmail.com>
2020-05-07 10:44:01 +0100

    Continued work on tests complete

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -1,10 +1,12 @@
 require './lib/fizzbuzz'
+
 describe 'fizzbuzz' do
 
-    let(:expected) {["1","2","fizz","4","buzz","fizz","7","8","fizz","buzz","11","fizz","13","14","fizzbuzz","16","17","fizz","19","buzz"].join("\n")}
-
-    it 'returns "1" when passed 1' do
-        expect(fizzbuzz(1)).to eq 1
+    
+    [1,2,4].each do |element| 
+        it "returns #{element} when given #{element}" do
+        expect(fizzbuzz(element)).to eq element
+        end
     end
 
     it 'returns "fizz" when passed 3' do
@@ -17,9 +19,14 @@ describe 'fizzbuzz' do
 
     it 'returns "fizzbuzz" when passed a multiple of 3 and 5' do 
         expect(fizzbuzz(15)).to eq 'fizzbuzz'
-    end 
+    end
+end
 
-    it "Puts'ed number 1-2 to output each on a new line" do
+describe "fizzbuzz printing" do
+
+let(:expected) {["1","2","fizz","4","buzz","fizz","7","8","fizz","buzz","11","fizz","13","14","fizzbuzz","16","17","fizz","19","buzz"].join("\n")}
+
+    it "Puts'ed number 1-20 to output each on a new line" do
         expect{complete_fizz}.to output(/#{expected}/).to_stdout
     end
 
